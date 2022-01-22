d6edabf292ab861f02a6ec63698720dc1ceea3a8
HARRISH_G <98208497+BLOCKCHAIN-MONSTER@users.noreply.github.com>
2022-01-22 14:25:45 +0530

    Update index.py

--- index.py
+++ index.py
@@ -1,15 +1,17 @@
 from web3 import Web3
 
-
-w3 = Web3(Web3.HTTPProvider("https://mainnet.infura.io/v3/58ca44ea40d34cb7939ccaa8c9adf06e"))
+#CONNECT TO WEB3
+w3 = Web3(Web3.HTTPProvider(YOUR INFURA MAINET URL))
 if w3:
     print('connected')
+    
+#ABI OF BOTH UNISWAP AND KYBERSWAP POOL
 uni_abi = open("Uniswappoolabi.txt", "r").read()
 kyber_abi = open('kyberswappoolabi.txt','r').read()
 
-
+#POOL ADDRESS FOR BOTH UNISWAP AND KYBERSWAP
 pool_address = {
-
+  #Dai is thestable coin which worth for $1   
   'Dai_to_USDT' : ['0xB20bd5D04BE54f870D5C0d3cA85d82b34B836405', '0x8e6dBCaC80Be6E4ef4bB2ceCD83ABc41a1E968e2'],
 
   'Dai_to_USDC'  : ['0xAE461cA67B15dc8dc81CE7615e0320dA1A9aB8D5','0xa7b64388f0f125354A3C5d5f799EBcDf1F832419']
@@ -31,6 +33,7 @@ for name , address in pool_address.items():
 
     difference = (uni_price - kyber_price)
     addition  = uni_price + kyber_price
+    #addition*0.005 is the gas fee for a dual swap
     if difference <= addition*0.005 and difference >= 0:
         profit = False
     elif difference <= addition*0.005*(-1) and difference <= 0:
@@ -38,4 +41,4 @@ for name , address in pool_address.items():
     else:
         profit = True
     print(f"profit :{profit}")
-    print(difference)
+    
